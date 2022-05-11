4f8f2226163d001b4cc36d8b1ab7cdced6a58fdf
Danila <danpry@st.amu.edu.pl>
2022-05-11 18:20:26 +0200

    There is given a rectangular bitmap of size n*m. Each pixel of the bitmap is either white or black, but at least one is white. The pixel in i-th line and j-th column is called the pixel (i,j). The distance between two pixels p1=(i1,j1) and p2=(i2,j2) is defined as:
    d(p1,p2)=|i1-i2|+|j1-j2|.
    
    Task
    Write a program which:
    
    reads the description of the bitmap from the standard input,
    for each pixel, computes the distance to the nearest white pixel,
    writes the results to the standard output.
    
    Input
    The number of test cases t is in the first line of input, then t test 
    cases follow separated by an empty line. In the first line of each test 
    case there is a pair of integer numbers n, m separated by a single 
    space, 1<=n <=182, 1<=m<=182. In each of the following n lines of the 
    test case exactly one zero-one word of length m, the description of one 
    line of the bitmap, is written. On the j-th position in the line (i+1), 
    1 <= i <= n, 1 <= j <= m, is '1' if, and only if the pixel (i,j) is 
    white.
    
    Output
    In the i-th line for each test case, 1<=i<=n, there should be written m 
    integers f(i,1),...,f(i,m) separated by single spaces, where f(i,j) is 
    the distance from the pixel (i,j) to the nearest white pixel.

---
+++ Efficient Algorithms course/c++/Bitmap.cpp
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <queue>
+
+// ---------------------------   GLOBALE  --------------------------- //
+std::queue<std::pair<short int, short int> >Q;
+int ltestow, h, w, w_a, h_a, tab[200][200];
+char c;
+
+// ---------------------------  FUNCTIONS --------------------------- //
+void READ();
+void PRINT();
+void BFS();
+void START_Q();
+void CLEAR();
+// ---------------------------    MAIN    --------------------------- //
+int main ()
+{
+    scanf("%d", &ltestow);
+    while(ltestow--)
+    {
+        CLEAR();
+        READ();
+        BFS();
+        PRINT();
+    }
+
+}
+
+// ---------------------------  FUNCTIONS --------------------------- //
+void READ()
+{
+    while(Q.size())
+        Q.pop();
+    scanf("%d%d ", &h, &w);
+    for(int i=0; i<h; i++)
+    {
+        for(int j=0; j<w; j++)
+        {
+            scanf("%c", &c);
+            if(c=='1')
+            {
+                tab[j][i]=0;
+                Q.push(std::make_pair(j, i));
+            }
+        }
+        scanf("\n");
+    }
+}
+void PRINT()
+{
+    for(int i=0; i<h; i++)
+    {
+        for(int j=0; j<w; j++)
+            printf("%d ", tab[j][i]);
+        printf("\n");
+    }
+}
+void BFS()
+{
+    while(Q.size())
+    {
+        w_a=Q.front().first;
+        h_a=Q.front().second;
+// SZEROKOŚĆ
+        if(w_a>0 && tab[w_a-1][h_a]==-1)
+        {
+                tab[w_a-1][h_a]=tab[w_a][h_a]+1;
+                Q.push(std::make_pair(w_a-1, h_a));
+        }
+        if(w_a<w && tab[w_a+1][h_a]==-1)
+        {
+                tab[w_a+1][h_a]=tab[w_a][h_a]+1;
+                Q.push(std::make_pair(w_a+1, h_a));
+        }
+// WYSOKOŚĆ
+        if(h_a>0 && tab[w_a][h_a-1]==-1)
+        {
+                tab[w_a][h_a-1]=tab[w_a][h_a]+1;
+                Q.push(std::make_pair(w_a, h_a-1));
+        }
+        if(h_a<h && tab[w_a][h_a+1]==-1)
+        {
+                tab[w_a][h_a+1]=tab[w_a][h_a]+1;
+                Q.push(std::make_pair(w_a, h_a+1));
+        }
+        Q.pop();
+    }
+}
+void CLEAR()
+{
+    for(int i=0; i<200; i++)
+        for(int j=0; j<200; j++)
+            tab[j][i]=-1;
+}
